14917c7626153823040635a6bdacfd570e3ddaa3
Saulo <saulohgn@gmail.com>
2024-04-23 18:46:11 -0300

    fix dependencias

--- src/app/components/tarefas/tarefas.component.ts
+++ src/app/components/tarefas/tarefas.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { HlmTableDirective } from '@spartan-ng/ui-table-helm';
-import { HlmTableComponent } from '../spartan/ui-table-helm/src/lib/hlm-table.component';
-import { HlmCaptionComponent } from '../spartan/ui-table-helm/src/lib/hlm-caption.component';
-import { HlmTrowComponent } from '../spartan/ui-table-helm/src/lib/hlm-trow.component';
-import { HlmThComponent } from '../spartan/ui-table-helm/src/lib/hlm-th.component';
-import { HlmTdComponent } from '../spartan/ui-table-helm/src/lib/hlm-td.component';
+// import { HlmTableDirective } from '@spartan-ng/ui-table-helm';
+// import { HlmTableComponent } from '../spartan/ui-table-helm/src/lib/hlm-table.component';
+// import { HlmCaptionComponent } from '../spartan/ui-table-helm/src/lib/hlm-caption.component';
+// import { HlmTrowComponent } from '../spartan/ui-table-helm/src/lib/hlm-trow.component';
+// import { HlmThComponent } from '../spartan/ui-table-helm/src/lib/hlm-th.component';
+// import { HlmTdComponent } from '../spartan/ui-table-helm/src/lib/hlm-td.component';
 
 @Component({
   selector: 'app-tarefas',
@@ -12,12 +12,12 @@ import { HlmTdComponent } from '../spartan/ui-table-helm/src/lib/hlm-td.componen
   templateUrl: './tarefas.component.html',
   styleUrl: './tarefas.component.css',
   imports: [
-    HlmTableDirective,
-    HlmTableComponent,
-    HlmCaptionComponent,
-    HlmTrowComponent,
-    HlmThComponent,
-    HlmTdComponent,
+    // HlmTableDirective,
+    // HlmTableComponent,
+    // HlmCaptionComponent,
+    // HlmTrowComponent,
+    // HlmThComponent,
+    // HlmTdComponent,
   ],
 })
 export class TarefasComponent {}
